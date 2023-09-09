48d2347525105477013ba4167061310ada8a3ea7
manjula180626 <144505669+manjula180626@users.noreply.github.com>
2023-09-09 08:48:56 +0530

    Add files via upload

---
+++ challenge 1.1/main (1).py
@@ -0,0 +1,11 @@
+#implement a recursive function to calculate the factorial of a given number.
+# Factorial of a number using recursion
+def recur_factorial(n):
+ if n==0 or n==1:
+    return 1 
+  else:
+  return n*recur_factorial(n-1)
+    return n*recur_factorial(n-1)
+num=int(input("Enter a number:"))
+result=recur_factorial(num)
+print("The factorial of {} is {}.".format(num,result))
